0f4b6a380a9634b86ce9a4895aaedfab6101cac0
Mortis66666 <mortisloverbs@gmail.com>
2024-01-10 17:05:57 +0800

    functioning questions

--- src/lib/components/Question.svelte
+++ src/lib/components/Question.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+    import { getQuestion } from "$lib/data/fetchData";
+    import type { Question } from "$lib/data/types";
+
     export let questions: Array<string | any>;
 
     console.log(questions);
 
-    let currentQuestion;
+    let current = 0;
+    let question: Question;
+    $: question = getQuestion(questions[current]);
 </script>
 
-<p>Question is :{questions}</p>
+{#if question.questionType == "vocab"}
+    <p>What is the definition of the following word?</p>
+{/if}
+
+<p>{question.question}</p>
+
+{#if question.answerType == "objective"}
+    {#each question.choices as choice}
+        <button>{choice}</button>
+    {/each}
+{/if}
--- src/lib/data/fetchData.ts
+++ src/lib/data/fetchData.ts
@@ -1,32 +1,31 @@
 import bank from "./bank.json";
 import definitions from "./definitions.json";
-import type { Result } from "./types";
+import questions from "./questions.json";
+import type { Question, Result } from "./types";
 
 const definitionsDict: { [key: string]: any } = definitions;
+const questionsDict: { [key: string]: Question } = questions;
 
 export function getDefinition(lang: any, word: any) {
     return definitionsDict[lang][word];
 }
 
 export function findData(query: string | undefined, data: Array<Result | string | any> = bank) {
-    console.log(data);
-    console.log(query);
-
     if (!query) return data as Array<Result>;
 
     let result: Result = data.find((item: any) => item.id === query) || {};
-    console.log(result);
 
     return result.childs as Array<Result | string>;
 }
 
 export function recursiveFind(routeList: Array<string>, data: Array<Result | string | any> = bank) {
-    console.log(routeList);
-
     let currentData = findData(routeList.shift(), data);
-    console.log(currentData);
 
     if (routeList.length === 0) return currentData;
 
     return recursiveFind(routeList, currentData);
+}
+
+export function getQuestion(id: string) {
+    return questionsDict[id];
 }
