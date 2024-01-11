01ef4fc03ae06423178bb043ee84339916173be0
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-01-11 16:23:14 +0800

    optimization

--- src/routes/home/[language]/[form]/[subject]/+page.svelte
+++ src/routes/home/[language]/[form]/[subject]/+page.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import Buttons from "$lib/components/Button.svelte";
-    import Question from "$lib/components/Question.svelte";
-    import { page } from "$app/stores";
     import type { PageData } from "./$types";
-    import { onMount } from "svelte";
     import Button from "$lib/components/Button.svelte";
 
     export let data: PageData;
-    console.log(data);
-    let buttons: any, button: any;
 
-    $: buttons = data.buttons;
-    $: ({ id, title, child } = buttons[0]);
-
-    $: console.log(id, title);
+    $: ({ buttons } = data);
+    $: ({ id, title } = buttons[0]);
 </script>
 
 <main>
