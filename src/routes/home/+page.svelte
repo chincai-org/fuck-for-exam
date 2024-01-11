f7b12d1e5c47c01cd11b7f9033c3844a0eef90f0
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-01-11 13:36:51 +0800

    route system implemented

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import Buttons from "$lib/components/Button.svelte";
+    import Question from "$lib/components/Question.svelte";
+    import { page } from "$app/stores";
+    import type { PageData } from "./$types";
+    import { onMount } from "svelte";
+    import Button from "$lib/components/Button.svelte";
+
+    let title = "never gonna give you up";
+    let id = "zh";
+</script>
+
+<main>
+    <Button {title} {id} />
+</main>
---
+++ src/routes/home/[language]/[form]/+layout.ts
@@ -0,0 +1,20 @@
+import type { PageLoad } from "./$types";
+import { findData, recursiveFind } from "$lib/data/fetchData";
+
+export const load: any = (({ params }: any) => {
+    console.log(window.location.href);
+    console.log(params);
+
+    const rawRoutes: string = window.location.href.replace(/https?:\/\/[^\/]+\//, "");
+    const routeList: Array<string> = rawRoutes.split("/");
+    console.log(routeList);
+    const buttons = recursiveFind(routeList);
+
+    console.log(buttons);
+
+    console.log(`/${params.language}`);
+
+    return {
+        buttons: buttons,
+    };
+}) satisfies PageLoad;
