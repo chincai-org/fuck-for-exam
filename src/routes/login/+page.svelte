c8dd2003ba72013fe715926311cbac4320f3c54d
Mortis66666 <mortisloverbs@gmail.com>
2024-01-17 23:03:41 +0800

    temporaly moved start page to /start_page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,11 +4,4 @@
     export let data: PageData;
 </script>
 
-<main>
-    <h2>Welcome to</h2>
-    <h1>FUCK</h1>
-    <h3>where you get</h3>
-    <h3>Free Unlimited Comprehensive Knowledge</h3>
-    <button>Sign up</button>
-    <button>Login</button>
-</main>
+<main>hello</main>
---
+++ src/routes/start_page/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+</script>
+
+<main>
+    <h2>Welcome to</h2>
+    <h1>FUCK</h1>
+    <h3>where you get</h3>
+    <h3>Free Unlimited Comprehensive Knowledge</h3>
+    <button>Sign up</button>
+    <button>Login</button>
+</main>
