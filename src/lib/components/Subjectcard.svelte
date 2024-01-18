cc120b7f1553de20fef7ffbd5a3917271c475089
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-01-18 15:54:21 +0800

    did not commit all

---
+++ src/lib/components/Subjectcard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let cards: any;
+</script>
+
+{#each cards as card}
+    <div class="--card">
+        <h1 class="--card__heading">{card.title}</h1>
+        <hr class="--bg-neutral-1000" />
+        <span class="--clr-neutral-500">Continue lesson Topic {card.lastDoneTopic}</span>
+    </div>
+{/each}
+
+<style lang="scss">
+    hr {
+        border: 0;
+        border-radius: 100vw;
+        height: 2px;
+    }
+</style>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import Buttons from "$lib/components/Button.svelte";
     import Question from "$lib/components/Question.svelte";
     import { page } from "$app/stores";
     import type { PageData } from "./$types";
     import { onMount } from "svelte";
     import Button from "$lib/components/Button.svelte";
+    import Subjectcard from "$lib/components/Subjectcard.svelte";
 
     let title = "why lang first";
     let id = "zh";
@@ -22,21 +22,7 @@
 
 <main>
     <div class="--grid-auto-fit">
-        {#each cards as card}
-            <div class="--card">
-                <div class="--card__heading">{card.title}</div>
-                <hr class="--bg-neutral-1000" />
-                <span class="--clr-neutral-500">Continue lesson Topic {card.lastDoneTopic}</span>
-            </div>
-        {/each}
+        <Subjectcard {cards} />
     </div>
     <Button {title} {id} />
 </main>
-
-<style lang="scss">
-    hr {
-        border: 0;
-        border-radius: 100vw;
-        height: 2px;
-    }
-</style>
