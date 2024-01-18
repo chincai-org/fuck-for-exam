f7e1b46612b0a9eddd98f450d15d81d27bd223dc
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-01-18 15:53:37 +0800

    subject card added, make componet

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -9,8 +9,34 @@
 
     let title = "why lang first";
     let id = "zh";
+
+    const cards = [
+        { title: "Sejarah", lastDoneTopic: "12.2" },
+        { title: "Math", lastDoneTopic: "2.2" },
+        { title: "Science", lastDoneTopic: "1.2" },
+        { title: "Sejarah", lastDoneTopic: "12.2" },
+        { title: "Math", lastDoneTopic: "2.2" },
+        { title: "Science", lastDoneTopic: "1.2" }
+    ];
 </script>
 
 <main>
+    <div class="--grid-auto-fit">
+        {#each cards as card}
+            <div class="--card">
+                <div class="--card__heading">{card.title}</div>
+                <hr class="--bg-neutral-1000" />
+                <span class="--clr-neutral-500">Continue lesson Topic {card.lastDoneTopic}</span>
+            </div>
+        {/each}
+    </div>
     <Button {title} {id} />
 </main>
+
+<style lang="scss">
+    hr {
+        border: 0;
+        border-radius: 100vw;
+        height: 2px;
+    }
+</style>
