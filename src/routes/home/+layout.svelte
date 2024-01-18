beef4f62d22dffea084e504304e32d0451bcbfbc
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-01-18 14:34:39 +0800

    top bar added

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -30,82 +30,105 @@
 </script>
 
 <main class="--bg-neutral-100">
-    <nav>
-        <ul class="--nav">
-            {#each navigations as navigation}
-                <li class:active={$page.url.pathname === navigation.path}>
-                    <i class={navigation.icon} />
-                    <a href={navigation.path}>{navigation.title}</a>
-                </li>
-            {/each}
-        </ul>
-    </nav>
-    <slot />
+    <header class="--site-header">
+        <div class="--container" data-container="wide">
+            <div class="--site-header__inner">
+                <p>@Hoot Hoot please say what you want to add at the top bar here</p>
+                <nav>
+                    <ul class="--nav | --flex-group">
+                        <li>
+                            <a href="">What</a>
+                        </li>
+                        <li>
+                            <a href="">What</a>
+                        </li>
+                        <li>
+                            <a href="">What</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section>
+        <nav>
+            <ul class="--nav">
+                {#each navigations as navigation}
+                    <li class:active={$page.url.pathname === navigation.path}>
+                        <i class={navigation.icon} />
+                        <a href={navigation.path}>{navigation.title}</a>
+                    </li>
+                {/each}
+            </ul>
+        </nav>
+        <slot />
+    </section>
 </main>
 
 <style lang="scss">
     @use "/src/lib/sass/abstracts/" as *;
 
-    main {
-        min-height: 100vh;
+    section {
+        padding-left: 0.5rem;
+        padding-block: 1rem;
         display: grid;
         grid-template-columns: 18rem 1fr;
         gap: 2rem;
-    }
 
-    nav {
-        background: $color-neutral-200;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        max-height: 100svh;
-        position: sticky;
-        padding-block: 2rem;
-    }
+        nav {
+            border-radius: 2vw;
+            background: $color-neutral-200;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            max-height: 100svh;
+            position: sticky;
+            padding-block: 3.5rem;
+            ul {
+                li {
+                    display: grid;
+                    padding-block: 1rem;
+                    padding-inline: 2rem;
+                    margin-inline: 1rem 0;
+                    box-shadow: 2px 0 0 $color-neutral-100;
+                    grid-template-columns: 3rem 1fr;
 
-    ul {
-        li {
-            display: grid;
-            padding-block: 1rem;
-            padding-inline: 2rem;
-            margin-inline: 1rem 0;
-            box-shadow: 2px 0 0 $color-neutral-100;
-            grid-template-columns: 3rem 1fr;
+                    &.active {
+                        $border-radius: 2rem;
 
-            &.active {
-                $border-radius: 3rem;
+                        view-transition-name: navigation;
+                        background-color: $color-neutral-100;
+                        position: relative;
+                        z-index: -1;
+                        border-radius: 100vw 0 0 100vw;
 
-                view-transition-name: navigation;
-                background-color: $color-neutral-100;
-                position: relative;
-                z-index: -1;
-                border-radius: 100vw 0 0 100vw;
+                        @include before-after($locations: before after, $right: 0) {
+                            width: $border-radius;
+                            height: $border-radius;
+                            background: $color-neutral-200;
+                        }
 
-                @include before-after($locations: before after, $right: 0) {
-                    width: $border-radius;
-                    height: $border-radius;
-                    background: $color-neutral-200;
-                }
+                        @include before-after($locations: before, $top: $border-radius * -1) {
+                            border-radius: 0 0 $border-radius;
+                            box-shadow: calc($border-radius / 6) calc($border-radius / 6) 0
+                                calc($border-radius / 6) $color-neutral-100;
+                        }
 
-                @include before-after($locations: before, $top: $border-radius * -1) {
-                    border-radius: 0 0 $border-radius;
-                    box-shadow: calc($border-radius / 6) calc($border-radius / 6) 0
-                        calc($border-radius / 6) $color-neutral-100;
-                }
+                        @include before-after($locations: after, $bottom: $border-radius * -1) {
+                            border-radius: 0 $border-radius 0 0;
+                            box-shadow: calc($border-radius / 6) calc(($border-radius / -6)) 0
+                                calc($border-radius / 6) $color-neutral-100;
+                        }
+                    }
 
-                @include before-after($locations: after, $bottom: $border-radius * -1) {
-                    border-radius: 0 $border-radius 0 0;
-                    box-shadow: calc($border-radius / 6) calc(($border-radius / -6)) 0
-                        calc($border-radius / 6) $color-neutral-100;
-                }
-            }
-
-            @for $i from 1 through 4 {
-                &:nth-child(#{$i}) a {
-                    view-transition-name: navLink-#{$i};
-                }
-                &:nth-child(#{$i}) i {
-                    view-transition-name: navIcon-#{$i};
+                    @for $i from 1 through 4 {
+                        &:nth-child(#{$i}) a {
+                            view-transition-name: navLink-#{$i};
+                        }
+                        &:nth-child(#{$i}) i {
+                            view-transition-name: navIcon-#{$i};
+                        }
+                    }
                 }
             }
         }
